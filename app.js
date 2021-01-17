923eb7d579255f2f35a876cf4f1fdf1c1c282c4b
eliesefactory <75095850+eliesefactory@users.noreply.github.com>
2021-01-17 17:38:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
-const express = require('express');
-const path = require('path');
-const store = require('./store/datastore');
-const initialStoreData = require('./store/data');
-const Musician = require('./models/musician');
-const musicianRoutes = require('./routes/musician');
+console.log("hello2");
 
-const app = express();
-const port = process.env.PORT || 3001;
+var http = require('http');
 
-// include routes
-app.use('/musician', musicianRoutes);
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('hello 35');
+    response.end();
+}
 
-app.use(express.static('public'));
-
-// Index route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
-
-// initialize store
-const musician = new Musician(store);
-musician.initStore(initialStoreData);
-app.locals.musician = musician;
-
-// start server
-const server = app.listen(port, () => {
-  console.log("Server started on port " + port);
-});
-
-module.exports = server;
+http.createServer(onRequest).listen(8000);
